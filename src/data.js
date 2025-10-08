ca1b7a02df1b537398c7c6fdf04017d745c3cdf3
JashwantChohan <116147134+JashwantChohan@users.noreply.github.com>
2025-10-08 23:32:17 +0500

    changes

---
+++ src/data.js
@@ -0,0 +1,13 @@
+export const API_KEY = import.meta.env.VITE_API_KEY
+
+const value_converter = (value) => {
+    if (value >= 1000000) {
+        return Math.floor(value / 1000000) + 'M'
+    } else if (value >= 1000) {
+        return Math.floor(value / 1000) + 'k'
+    } else {
+        return value
+    }
+}
+
+export default value_converter
