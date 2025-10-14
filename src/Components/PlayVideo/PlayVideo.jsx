769a52d0e6f19c4507e1d3d94bbbdbc485f01dbf
JashwantChohan <116147134+JashwantChohan@users.noreply.github.com>
2025-10-14 22:45:30 +0500

    changes

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -8,6 +8,7 @@ import save from '../../assets/save.png'
 import jack from '../../assets/jack.png'
 import user_profile from '../../assets/user_profile.jpg'
 import value_converter, { API_KEY } from '../../data'
+import moment from 'moment'
 
 function PlayVideo({ videoId }) {
   const [apiData, setAipiData] = useState(null)
@@ -15,21 +16,24 @@ function PlayVideo({ videoId }) {
   const fetchVideoData = async () => {
     // Fetching videos data
     const videoDetails_url = ` https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`
-    await fetch(videoDetails_url).then(res => res.json()).then(data => setAipiData(data.items[0]))
+    await fetch(videoDetails_url)
+      .then(res => res.json())
+      .then(data => setAipiData(data.items[0]))
   }
 
   useEffect(() => {
     fetchVideoData()
   }, [])
 
+//  2:19:49
 
   return (
     <div className='play-video'>
       {/* <video src={video1} controls autoPlay muted></video> */}
       <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-      <h3>{apiData?apiData.snippet.title:"Title here"}</h3>
+      <h3>{apiData ? apiData.snippet.title : "Title here"}</h3>
       <div className="play-video-info">
-        <p>{apiData?value_converter(apiData.statistics.viewCount): "16K"} &bull; 2 days ago</p>
+        <p>{apiData ? value_converter(apiData.statistics.viewCount) : "16K"} &bull; apiData?{moment(apiData.snippet.publishedAt).fromNow()}: ""</p>
         <div className="">
           <span><img src={like} alt="" />125</span>
           <span><img src={dislike} alt="" />2</span>
